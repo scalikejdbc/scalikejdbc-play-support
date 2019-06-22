93ef47a042795cc493a268a0ecc2e11c8371ce6d
kenji yoshida <6b656e6a69@gmail.com>
2019-06-22 09:53:32 +0900

    Update release.sh. add Scala 2.13

--- release.sh
+++ release.sh
@@ -10,4 +10,9 @@ sbt  ++2.12.8 \
     play-dbapi-adapter/publishSigned \
     play-fixture/publishSigned
 
+sbt  ++2.13.0 \
+    play-initializer/publishSigned \
+    play-dbapi-adapter/publishSigned \
+    play-fixture/publishSigned
+
 sbt sonatypeRelease # FIXME: doesn't work.. java.lang.IllegalStateException: No staging repository is found. Run publishSigned first
