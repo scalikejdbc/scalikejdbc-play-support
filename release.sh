bd9148ab2f58745df46133437f13ed9fef7f5c22
Kazuhiro Sera <seratch@gmail.com>
2019-12-13 15:52:35 +0900

    version version 2.8.0-scalikejdbc-3.4

--- release.sh
+++ release.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sbt ++2.11.12 \
-    play-initializer/publishSigned \
-    play-dbapi-adapter/publishSigned \
-    play-fixture/publishSigned
-
 sbt  ++2.12.10 \
     play-initializer/publishSigned \
     play-dbapi-adapter/publishSigned \
@@ -14,5 +9,3 @@ sbt  ++2.13.1 \
     play-initializer/publishSigned \
     play-dbapi-adapter/publishSigned \
     play-fixture/publishSigned
-
-# sbt sonatypeRelease # FIXME: doesn't work.. java.lang.IllegalStateException: No staging repository is found. Run publishSigned first
